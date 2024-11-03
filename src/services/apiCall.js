4d445cd9192a2f68c71218df3b93b6970353213a
Navaneeth P Kotian <navaneeth.kotian@niveussolutions.com>
2024-11-03 08:08:38 +0530

    api calls configuration

---
+++ src/services/apiCall.js
@@ -0,0 +1,13 @@
+import axios from "./apiConfig";
+import { API_ENDPOINTS } from "../constants/apiEndpoints";
+
+export const AuthDetails = async (payload) => {
+  try {
+    const response = await axios.post(API_ENDPOINTS.AUTH.signup, payload);
+    if (response) {
+      return response;
+    }
+  } catch (error) {
+    return error?.response;
+  }
+};
